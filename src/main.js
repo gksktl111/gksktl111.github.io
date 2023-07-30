c39f41aca5f27e657e4cf7d51699a0567f0c2162
gksktl111 <gksktl111@naver.com>
2023-07-31 02:16:51 +0900

    JS-Menu

--- src/main.js
+++ src/main.js
@@ -8,6 +8,8 @@ const $homeTitle = document.querySelector('.home__title');
 const $homeDescription = document.querySelector('.home__description');
 const $homeContact = document.querySelector('.home__contact');
 
+const $arrow = document.querySelector('.arrow-up');
+
 window.addEventListener('scroll', () => {
   if (window.scrollY > headerHeight) {
     $header.classList.add('header--dark');
@@ -23,4 +25,26 @@ window.addEventListener('scroll', () => {
   $homeTitle.style.opacity = `${opacity}`;
   $homeDescription.style.opacity = `${opacity}`;
   $homeContact.style.opacity = `${opacity}`;
+});
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > homeHeight / 2) {
+    $arrow.classList.add('arrow--up--visible');
+  } else {
+    $arrow.classList.remove('arrow--up--visible');
+  }
+});
+
+// navbar 토글버튼 클릭 처리
+const navBarMenu = document.querySelector('.header__menu');
+const navbarToggle = document.querySelector('.header__toggle');
+
+navbarToggle.addEventListener('click', () => {
+  navBarMenu.classList.toggle('open');
+});
+
+// Navbar 메뉴 클릭시 메뉴를 자동으로 닫아줌
+navBarMenu.addEventListener('click', (event) => {
+  // if (event.target.tagName !== 'A') return;
+  navBarMenu.classList.remove('open');
 });
