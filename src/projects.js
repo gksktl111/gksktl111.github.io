3a37f60a59518ea57db35e52b6142b17e3116ddc
gksktl111 <gksktl111@naver.com>
2023-08-10 01:17:41 +0900

    Update projects.js

--- src/projects.js
+++ src/projects.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// 프로젝트 필터링 관련 로직 처리
+// 프로젝트 필터링 관련 로직 처리.
 const categories = document.querySelector('.categories');
 const projects = document.querySelectorAll('.project');
 const projectContainer = document.querySelector('.projects');
